89059459f72b69599214b53986f9d7498fc6c5eb
John6tain <john6tain.tube@gmail.com>
2016-01-28 00:02:51 +0200

    Fixed Form Name and Size

---
+++ Poker/PokerTest/Shuffle/SetupGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace PokerTest.Shuffle
+{
+    [TestClass]
+    public class SetupGame
+    {
+       
+
+        [TestMethod]
+        public void Test_SetupGame()
+        {
+            //Mock<> 
+
+        }
+    }
+}
