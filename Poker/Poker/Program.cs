413cfb2a6a02a0df915f19e341205f857296031d
John6tain <john6tain.tube@gmail.com>
2016-01-28 00:01:10 +0200

    Fixed mord name and Size

--- Poker/Poker/Program.cs
+++ Poker/Poker/Program.cs
@@ -15,7 +15,7 @@ namespace Poker
             {
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new Form1());
+                Application.Run(new TexasPoker());
             }
             catch (Exception e)
             {
--- Poker/Poker/TexasPoker.cs
+++ Poker/Poker/TexasPoker.cs
@@ -23,7 +23,7 @@ using Poker.Table;
 
 namespace Poker
 {
-    public partial class Form1 : Form
+    public partial class TexasPoker : Form
     {
         private ICharacter bot1;
         private ICharacter bot2;
@@ -37,7 +37,7 @@ namespace Poker
         private IDealer dealer;
 
 
-        public Form1()
+        public TexasPoker()
         {
             InitializeComponent();
             InitializeGameObjects();
