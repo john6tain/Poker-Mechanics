422c19f773db57f67fac235d968044075026b763
Martinik <martin.nik@live.com>
2016-01-28 08:01:08 +0200

    try-catch in main with messagebox

--- Poker/Poker/Program.cs
+++ Poker/Poker/Program.cs
@@ -19,7 +19,8 @@ namespace Poker
             }
             catch (Exception e)
             {
-                //TODO: figure out this one ...
+                MessageBox.Show(e.Message.ToString(), "Exception Hass Occured", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
             }
         }
     }
